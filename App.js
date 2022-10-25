deb66fb2500f5d743264a931f843fa5fd8689f6a
Sazzad Hossain <sazzad15-2521@diu.edu.bd>
2022-10-25 13:41:19 +0600

    okk

--- App.js
+++ App.js
@@ -1,48 +1,92 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
-import LogIn from './Screen/Login';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import ForgetPassword from './Screen/ForgetPassword';
-import Register from './Screen/Register';
-import Home from './Screen/Home';
-import Search from './Screen/Search';
-import FoodDetails from './Screen/FoodDetails';
-import OrderConfirmation from './Screen/OrderConfirmation';
-import LocationSearch from './Screen/LocationSearch';
-import EditPtofile from './Screen/EditProfile';
-import DeviceLogin from './Screen/DeviceLogin';
-import ServiceStatus from './Screen/ServiceStatus';
-
-
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, ScrollView, SafeAreaView } from "react-native";
+import LogIn from "./Screen/Login";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import ForgetPassword from "./Screen/ForgetPassword";
+import Register from "./Screen/Register";
+import Home from "./Screen/Home";
+import Search from "./Screen/Search";
+import FoodDetails from "./Screen/FoodDetails";
+import OrderConfirmation from "./Screen/OrderConfirmation";
+import LocationSearch from "./Screen/LocationSearch";
+import EditPtofile from "./Screen/EditProfile";
+import DeviceLogin from "./Screen/DeviceLogin";
+import ServiceStatus from "./Screen/ServiceStatus";
 
 const Stack = createNativeStackNavigator();
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen options={{headerShown:false}} name="Log In" component={LogIn} />
-        <Stack.Screen options={{headerShown:false}} name="Forget Password" component={ForgetPassword} />
-        <Stack.Screen options={{headerShown:false}} name="Create new account" component={Register} />
-        <Stack.Screen options={{headerShown:false}} name="Home" component={Home} />
-        <Stack.Screen options={{headerShown:false}} name="Search" component={Search} />
-        <Stack.Screen options={{headerShown:false}} name="Food Details" component={FoodDetails} />
-        <Stack.Screen options={{headerShown:false}} name="Order Confirmation" component={OrderConfirmation} />
-        <Stack.Screen options={{headerShown:false}} name="LocationSearch" component={LocationSearch} />
-        <Stack.Screen options={{headerShown:false}} name="Edit Profile" component={EditPtofile} />
-        <Stack.Screen options={{headerShown:false}} name="Device Login" component={DeviceLogin} />
-        <Stack.Screen options={{headerShown:false}} name="Service Status" component={ServiceStatus} />
+    <SafeAreaView style={{ flex: 1 }}>
+      <StatusBar barStyle="auto" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name="Log In"
+            component={LogIn}
+          />
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name="Forget Password"
+            component={ForgetPassword}
+          />
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name="Create new account"
+            component={Register}
+          />
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name="Home"
+            component={Home}
+          />
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name="Search"
+            component={Search}
+          />
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name="Food Details"
+            component={FoodDetails}
+          />
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name="Order Confirmation"
+            component={OrderConfirmation}
+          />
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name="LocationSearch"
+            component={LocationSearch}
+          />
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name="Edit Profile"
+            component={EditPtofile}
+          />
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name="Device Login"
+            component={DeviceLogin}
+          />
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name="Service Status"
+            component={ServiceStatus}
+          />
         </Stack.Navigator>
-    </NavigationContainer>
+      </NavigationContainer>
+    </SafeAreaView>
   );
 }
-
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
